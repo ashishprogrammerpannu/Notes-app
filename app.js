347e7ce4ccec654c7ca484ae7164581d95e05ff8
ashish <pannuashish123@gmail.com>
2020-04-15 08:58:56 -0700

    Commiting the end project

---
+++ app.js
@@ -0,0 +1,110 @@
+//  Defining Global variables
+let input = document.querySelector(".input");
+let btn = document.getElementsByTagName("button")[0];
+let noteStorage = document.querySelector(".notes");
+let searchbtn = document.getElementsByClassName("search")[0];
+let notesObj;
+let nav = document.getElementsByTagName("nav")[0];
+let search = document.createElement('input');
+search.id = `search`
+let boolean = false;
+search.placeholder=`Search notes`;
+addNotes();
+btn.addEventListener("click", () => {
+  let value = input.value.trim();
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.push(value);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+
+  console.log(notesObj.length);
+  input.value = "";
+  addNotes();
+});
+
+function addNotes() {
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+
+  let html = ``;
+  notesObj.forEach(function (element, index) {
+    html += ` <div class="noteFather">
+                       <h2>${index + 1}.</h2>
+                        <p class="note">${element}</p>
+                        <button class="delete" id=${index} onclick = deleteNote(this.id)>Delete</button>
+                     </div>`;
+  });
+  if (notes != null) {
+    noteStorage.innerHTML = html;
+  }
+}
+
+function deleteNote(index) {
+  notesObj;
+  let notes = localStorage.getItem("notes");
+  if (notes == null) {
+    notesObj = [];
+  } else {
+    notesObj = JSON.parse(notes);
+  }
+  notesObj.splice(index, 1);
+  localStorage.setItem("notes", JSON.stringify(notesObj));
+
+  addNotes();
+}
+
+searchbtn.addEventListener('click',()=>{
+    nav.replaceChild(search,searchbtn);
+    boolean = 'Ok';
+})
+
+document.getElementsByClassName('input_container')[0].addEventListener('click',()=>{
+  if(boolean=='Ok'){
+  nav.replaceChild(searchbtn,search);
+  boolean=`notOk`
+}})
+
+search.addEventListener('input',()=>{
+  let value = search.value;
+  let txtContent = document.getElementsByClassName('noteFather');
+  Array.from(txtContent).forEach((element)=>{
+   let willBeShown = element.getElementsByTagName('p')[0].innerText.toLowerCase();
+   if(willBeShown.includes(value.trim().toLowerCase())){
+     element.style.display = 'block';
+   }
+   else{
+    element.style.display = 'none';
+   }
+  })
+})
+
+
+
+
+
+/*
+ Skipped portion
+function onclick(){
+    let value = input.value.trim();
+    let player = localStorage.length;
+    player++;
+    console.log(player);
+    localStorage.setItem(player,value);
+    let note = document.createElement('p');
+    note.textContent = `${localStorage.getItem(player)}`;
+    noteStorage.appendChild(note);
+}
+
+btn.addEventListener('click',onclick);
+btn.addEventListener('dblclick', ()=>{
+    localStorage.clear()
+});
+*/
